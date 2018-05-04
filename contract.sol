ac3b144a8fbc44e53ff3110b70a764b01b718926
Ramon <raimond_82@hotmail.com>
2018-05-04 03:54:42 +0200

    Contract

--- contract.sol
+++ contract.sol
@@ -2,13 +2,13 @@ pragma solidity ^0.4.16;
 
 interface tokenRecipient { function receiveApproval(address _from, uint256 _value, address _token, bytes _extraData) external; }
 
-contract TokenERC20 {
+contract DiscosToken {
     // Public variables of the token
-    string public name;
-    string public symbol;
+    string public name = "DiscosToken";
+    string public symbol = "DSC";
     uint8 public decimals = 18;
     // 18 decimals is the strongly suggested default, avoid changing it
-    uint256 public totalSupply;
+    uint256 public totalSupply = 24000;
 
     // This creates an array with all balances
     mapping (address => uint256) public balanceOf;
